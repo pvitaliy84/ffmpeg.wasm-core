0be49f8022318d0fe4c9494a583ede0f3bbd506d
Vitaliy Pozdnyakov <pvitaliy84@mail.ru>
2022-01-24 21:15:37 +0600

    smaller

--- wasm/build-scripts/var.sh
+++ wasm/build-scripts/var.sh
@@ -62,6 +62,8 @@ FFMPEG_CONFIG_FLAGS_BASE=(
   --disable-debug         # disable debug info, required by closure
   --disable-runtime-cpudetect   # disable runtime cpu detect
   --disable-autodetect    # disable external libraries auto detect
+  --disable-all
+  --disable-network
   --extra-cflags="$CFLAGS"
   --extra-cxxflags="$CFLAGS"
   --extra-ldflags="$LDFLAGS"
@@ -73,6 +75,7 @@ FFMPEG_CONFIG_FLAGS_BASE=(
   --cxx=em++
   --objcc=emcc
   --dep-cc=emcc
+
   ${EXTRA_FFMPEG_CONF_FLAGS-}
 )
 
